2340d942a84bb19be93b0de07f14339d27fca954
kate smith <kateesmith4994@gmail.com>
2020-12-17 15:07:32 +0000

    failing test to return 2020

---
+++ Day1/spec/Day1_spec.rb
@@ -0,0 +1,11 @@
+require "Day1"
+
+describe Run do
+   
+    context "returns 2 numbers that sum to 2020" do
+      it "returns 2020" do
+        expect(Run.sum).to eq(2020)
+       end
+    end
+      
+end
