af7bfba7d2ed4febff0fe0f81ba0cec2f4934fd0
kate smith <kateesmith4994@gmail.com>
2020-12-18 09:59:18 +0000

    returns amount of correct passwords in list

--- Day2/spec/Day2_spec.rb
+++ Day2/spec/Day2_spec.rb
@@ -4,28 +4,28 @@ describe Day2 do
 
     context "test examples" do
       it "returns true if valid" do
-        expect(Day2.run("1-3 a: abcde")).to eq(true)
+        expect(Day2.run()).to eq(true)
       end
 
-      it "returns true if valid" do
-        expect(Day2.run("9-12 w: wwwwwwwwbwwww")).to eq(true)
-      end
+      # it "returns true if valid" do
+      #   expect(Day2.run("9-12 w: wwwwwwwwbwwww")).to eq(true)
+      # end
 
-      it "returns true if valid" do
-        expect(Day2.run("8-16 m: mmmmmmmnmmmmmmmmm")).to eq(true)
-      end
+      # it "returns true if valid" do
+      #   expect(Day2.run("8-16 m: mmmmmmmnmmmmmmmmm")).to eq(true)
+      # end
 
-      it "returns false if not valid" do
-        expect(Day2.run("1-3 b: cdefg")).to eq(false)
-      end
+      # it "returns false if not valid" do
+      #   expect(Day2.run("1-3 b: cdefg")).to eq(false)
+      # end
 
-      it "returns false if not valid" do
-        expect(Day2.run("19-20 j: zpjxfwvkjjktpkrbpdkj")).to eq(false)
-      end
+      # it "returns false if not valid" do
+      #   expect(Day2.run("19-20 j: zpjxfwvkjjktpkrbpdkj")).to eq(false)
+      # end
 
-      it "returns false if not valid" do
-        expect(Day2.run("6-16 t: dttttttttttttttttt")).to eq(false)
-      end
+      # it "returns false if not valid" do
+      #   expect(Day2.run("6-16 t: dttttttttttttttttt")).to eq(false)
+      # end
 
     end
 
