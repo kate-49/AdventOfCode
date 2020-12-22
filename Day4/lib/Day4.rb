06fb9a2f493dd64c571d870d3ed386d97097ef9f
kate smith <kateesmith4994@gmail.com>
2020-12-22 11:08:31 +0000

    Returns valid if has 8 elements

--- Day4/lib/Day4.rb
+++ Day4/lib/Day4.rb
@@ -1,3 +1,10 @@
 class Day4
-   
+   def Day4.run(input)
+        array = input.split(" ")
+        if array.length == 8
+            return "Valid"
+        else 
+            return "Invalid"
+        end
+   end
 end
