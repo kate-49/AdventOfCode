a3e9484627ad13c26f92f4d712e89eeeb424b6f6
kate smith <kateesmith4994@gmail.com>
2020-12-22 09:47:11 +0000

    passes on longer example of real data

--- day3/spec/day3_spec.rb
+++ day3/spec/day3_spec.rb
@@ -4,25 +4,43 @@ describe Day3 do
 
     context "test examples" do
         it "tests function 1" do
-            expect(Day3.R1D1()).to eq(2)
+            expect(Day3.R1D1()).to eq(5)
         end
 
         it "tests function 2" do
-            expect(Day3.R3D1()).to eq(10)
+            expect(Day3.R3D1()).to eq(24)
         end
 
         it "tests function 3" do
-            expect(Day3.R5D1()).to eq(4)
+            expect(Day3.R5D1()).to eq(7)
         end
 
        
        it "tests function 4" do
-          expect(Day3.R7D1()).to eq(0)
+          expect(Day3.R7D1()).to eq(5)
         end
 
         it "tests function 5" do
-            expect(Day3.R1D2()).to eq(2)
+            expect(Day3.R1D2()).to eq(4)
         end
         
     end
+   
 end
+=begin
+TEST1 = "
+.........#....#.###.........##.
+..##.#......#......#.......##.
+##....#.#.......#.....#........
+#........#..........#.#...#...#
+#....###...##.....#........#...
+###..............##..#.....#...
+.................##.#..........
+.........##......#..###.....#.#
+..#..#...#.#.#...#.#.#.##...#..
+..............#.#.#..#..#..#...
+.#.#.#....#.........#.........#
+..#.#....##..#...#.....#..##..#
+............#.....#.........##.
+"
+=end
