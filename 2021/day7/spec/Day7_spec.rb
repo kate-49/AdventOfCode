748eae8bbcbaf7ea902d18dd677b1fa2f4efdb83
Kate <kate.smith@digital.justice.gov.uk>
2021-12-07 11:24:33 +0000

    day 7 part 1 complete

---
+++ 2021/day7/spec/Day7_spec.rb
@@ -0,0 +1,23 @@
+require "Day7Pt1"
+require "Day7Pt2"
+
+describe Day7Pt1 do
+
+    context "test examples" do
+      it "returns true if valid" do
+        expect(Day7Pt1.run()).to eq(37)
+      end
+    end
+end
+
+
+# describe Day7Pt2 do
+
+#   context "test examples" do
+#     it "returns true if valid" do
+#       expect(Day7Pt2.run()).to eq(1190)
+#     end
+
+#   end
+
+# end
