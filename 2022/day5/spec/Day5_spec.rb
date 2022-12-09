b6b511a153c37bbf86290ff64e7f6dba38543e50
Kate <kate.smith@digital.justice.gov.uk>
2022-12-09 12:47:25 +0000

    day 5 pt 2

--- 2022/day5/spec/Day5_spec.rb
+++ 2022/day5/spec/Day5_spec.rb
@@ -1,22 +1,22 @@
 require "Day5Pt1"
 require "Day5Pt2"
 
-describe Day5Pt1 do
+# describe Day5Pt1 do
 
-    context "test examples" do
-      it "returns true if valid" do
-        expect(Day5Pt1.run()).to eq("CMZ")
-      end
-    end
-end
-
-# describe Day5Pt2 do
-
-#   context "test examples" do
-#     it "returns true if valid" do
-#       expect(Day5Pt2.run()).to eq(4)
+#     context "test examples" do
+#       it "returns true if valid" do
+#         expect(Day5Pt1.run()).to eq("CMZ")
+#       end
 #     end
-
-#   end
-
 # end
+
+describe Day5Pt2 do
+
+  context "test examples" do
+    it "returns true if valid" do
+      expect(Day5Pt2.run()).to eq("MCD")
+    end
+
+  end
+
+end
